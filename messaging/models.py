82f945274d854fe5bea8bd8c6cb7cc6b9da9244d
Rasheed Babatunde <brasheed240@gmail.com>
2024-12-31 11:42:31 +0100

    Conversation model

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.utils import timezone
+import uuid
+
+
+
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='conversations',
+        through='ConversationParticipant'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['created_at']),
+            models.Index(fields=['updated_at']),
+        ]
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        super().save(*args, **kwargs)
